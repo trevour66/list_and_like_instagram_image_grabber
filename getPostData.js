4344709a2d9e38098d3afa32682420e49b891f45
Iniubong Peter <peteriniubong@gmail.com>
2024-11-20 19:31:46 +0100

    changed

--- getPostData.js
+++ getPostData.js
@@ -19,7 +19,7 @@ async function run() {
             { image_cdn: '' }
         ]
     };
-    const post = ig_profile_posts.find(query).limit(200);
+    const post = ig_profile_posts.find(query).limit(300);
 
     return post
 
